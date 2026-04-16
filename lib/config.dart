ef4835288e6fa4964c340c99ca7bfed6d3578a51
kagdi-abbas <kagdi.abbas1024@gmail.com>
2026-04-16 09:43:05 +0300

    .

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
-  static String key = "ck_f2ec2654805cf6a899054843b2ada1e422a9ea5f";
-  static String secret = "cs_2e24b5932edb4e555a2442658ab0e70887f22101";
-  static String apiURL = "code.com.kw";
+  static String key = "ck_XXX";
+  static String secret = "cs_XXX";
+  static String apiURL = "yoursite.com";
   static String apiEndPoint = "/wp-json/wc/v3/";
   static String categoriesURL = "products/categories";
 
